7c76d14d98a9c42235080187c3eb47321460e5f7
Igor <igorprozorow2000@gmail.com>
2021-08-28 16:53:53 +0300

    login test

---
+++ src/test/java/LoginPhoneBook.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginPhoneBook {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void init() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+        wd.manage().window().maximize();
+    }
+
+    @Test
+    public void loginTest() {
+        WebElement loginForm = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m :last-child"));
+        loginForm.click();
+        type(By.cssSelector("[placeholder='Email']"), "VASYA@mail.ru");
+        type(By.cssSelector("[placeholder='Password']"), "Awww123~");
+        WebElement loginButton = wd.findElement(By.cssSelector(".login_login__3EHKB :nth-child(4)"));
+        loginButton.click();
+
+    }
+
+    public void type(By locator, String text) {
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+
+    }
+
+
+}
